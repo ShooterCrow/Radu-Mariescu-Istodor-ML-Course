ccbbc61a1a73d6e2a0071f0a1556342d23588351
Victor Onyekwere <cydollarsign1@gmail.com>
2024-10-05 13:10:04 +0200

    Bla

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const canvas = document.querySelector("#canvas");
 const container = document.querySelector("#content");
-const userInputBox = document.querySelector("#username");
+const instructionDiv = document.querySelector(".instructionDiv");
+let userInputBox = document.querySelector("#username");
 
 const sp = new SketchPad(container, canvas, 300);
 const undoBtn = document.querySelector("#undoBtn");
+let index = 0;
+const itemsToDraw = ["car", "tree", "jug", "door", "tree", "hanger", "bulb", "cup", "bottle"]
 
 const data = {
     username: null,
@@ -17,7 +20,15 @@ function start (evt) {
         if (uname) {
             data.username = uname;
             userInputBox.value = ""
-            canvas.s
+            userInputBox.style.display = "none";
+            let instructions = document.createElement("p");
+            let nxtBtn = document.createElement("button");
+            nxtBtn.innerText = "Next";
+            nxtBtn.addEventListener("click", next)
+            instructions.innerText = "Please draw a " + itemsToDraw[index]
+            instructionDiv.appendChild(instructions)
+            instructionDiv.appendChild(nxtBtn)
+            userInputBox.innerHTML = "YYY";
             canvas.style.display = "block";
             undoBtn.style.display = "block";
         } else {
@@ -25,4 +36,8 @@ function start (evt) {
         }
 
     }
+}
+
+function next() {
+    alert()
 }
